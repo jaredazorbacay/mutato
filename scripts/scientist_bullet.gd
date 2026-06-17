eb1159513b793f21a97b3321386915e83f744cb4
YOUR NAME <jaredazorbacay@gmail.com>
2026-06-17 22:58:22 +0800

    Enlarging Orb

--- scripts/scientist_bullet.gd
+++ scripts/scientist_bullet.gd
@@ -10,9 +10,13 @@ func fire(direct, spd):
 	direction = direct
 	speed = spd
 	fired = true
+	$AnimatedSprite2D.play()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if (fired):
 		velocity = direction.normalized() * speed
 		move_and_slide()
+		if scale.x < 2:
+			scale.x += 0.9 * delta
+			scale.y += 0.9 * delta
