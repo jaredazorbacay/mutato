4821d446e84b6330fc27ad3a53a72dc6c370bfc0
constantineeeeee <examp23.lo23@gmail.com>
2026-06-20 18:10:46 +0800

    start other rooms

---
+++ scripts/rooms/room_2.gd
@@ -0,0 +1,22 @@
+extends TileMapLayer
+
+var right_door: Vector2i
+var left_door: Vector2i
+var top_door: Vector2i
+var bottom_door: Vector2i
+
+# position in world blocks
+var map_coords: Vector2i
+
+#position in tiles
+var tile_position_in_world: Vector2i
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#right_door = Vector2i(14,6)
+	#left_door = Vector2i(0, 0)
+	top_door = Vector2i(7,0)
+	bottom_door = Vector2i(7, 15)
+
+func set_coords(coords: Vector2i):
+	map_coords = coords
